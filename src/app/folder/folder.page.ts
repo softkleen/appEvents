0efeef77e30ba3eb3b201cbcc92db34abcc4457a
Wellington Vieira dos Santos <wellington.vsantos@gmail.com>
2022-09-13 11:28:30 -0300

    login completo

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -15,4 +15,5 @@ export class FolderPage implements OnInit {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  logout(){}
 }
---
+++ src/services/post.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import 'rxjs/add/operator/map';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  server: string = "http://localhost/uc11api91/";
+
+  constructor(private http: HttpClient) { }
+  dadosApi(dados:any, nomeApi: string){
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-type':'application/json'})
+    }
+    let url = this.server + nomeApi;
+    return this.http.post(url, JSON.stringify(dados),httpOptions).map(res => res)
+  }
+}
